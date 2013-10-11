e1a1688a564d37c6f2a252366376e16257238975
i.pavlov <i.pavlov@2gis.ru>
2013-10-11 18:01:59 +0700

    add wish publish form

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 class Wish(models.Model):
@@ -7,3 +8,7 @@ class Wish(models.Model):
 	status = models.CharField(max_length=200)
 	jira_ticket = models.CharField(max_length=200)
 	date = models.DateTimeField('date published')
+
+class WishForm(forms.Form):
+    email = forms.EmailField(max_length=100)
+    wish = forms.CharField()
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,41 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from dashboard.models import Wish
+from dashboard.models import Wish, WishForm
 
 def index(request):
 	wish_list = Wish.objects.order_by('date')[:5]
-	# template = loader.get_template('dashboard/index.html')
-	# context = RequestContext(request, {
-	#     'wish_list': wish_list,
-	# })
-	context = {
-		'wish_list': wish_list,
-	}
-	return render(request, 'dashboard/index.html', context)
-    # return HttpResponse(template.render(context))
+	
+	if request.method == 'POST': # If the form has been submitted...
+		form = WishForm(request.POST) # A form bound to the POST data
+		if form.is_valid(): # All validation rules pass
+			email = form.cleaned_data['email']
+			wish = form.cleaned_data['wish']
+
+			from django.utils import timezone
+			w = Wish(email=email, wish=wish, date=timezone.now())
+			w.save()
+			# from django.core.mail import send_mail
+			# send_mail("Your wish is our wish!", wish + "\n\nNow we know, thanks!", "autoqa@2gis.ru", [email])
+			return HttpResponseRedirect('/dashboard/') # Redirect after POST
+		else:
+			context = {
+				'form': form,
+				'wish_list': wish_list,
+			}
+		return render(request, 'dashboard/index.html', context)
+			
+		#return HttpResponse("there's an error")
+	else:
+		form = WishForm() # An unbound form
+		context = {
+			'form': form,
+			'wish_list': wish_list,
+		}
+		return render(request, 'dashboard/index.html', context)
+
+def thanks(request):
+	pass
